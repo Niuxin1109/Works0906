e063059d29a7c9ed89a78ca7a3ee69c2d9669176
Niuxin1109 <1409202584@qq.com>
2020-09-06 16:38:38 +0800

    Weekworks

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,26 +1,10 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import abc from '@/actions/home'
-import './styles.less'
+import React, { Component } from 'react'
 
-export default @connect(state => ({
-  data: state.home.data,
-}), {
-  ddd: abc.fetchHomeTags,
-})
-class Home extends React.PureComponent {
-  state = {
-    name: '小花'
-  }
-
-  componentDidMount () {
-    setTimeout(this.props.ddd, 5000)
-  }
-  
-  render () {
+export default class Home extends Component {
+  render() {
     return (
       <div>
-        {JSON.stringify(this.props.data)}
+        Home页面
       </div>
     )
   }
